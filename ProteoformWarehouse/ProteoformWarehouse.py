ab9e6e091c3e34538a6188f405c6a1922eba9668
rubenturner <67194782+rubenturner@users.noreply.github.com>
2021-05-03 00:08:24 -0400

    Updated files

---
+++ ProteoformWarehouse/ProteoformWarehouse.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# This Python file uses the following encoding: utf-8
+import sys
+import os
+import requests
+import csv
+import pprint
+
+import PyQt5
+
+from PyQt5.QtCore import *
+
+from PySide2 import QtCore
+from PySide2.QtWidgets import QApplication, QWidget, QTableWidget, QPushButton, QFileDialog, QListWidget
+from PySide2.QtCore import QFile
+from PySide2.QtUiTools import QUiLoader
+
+class ProteoformWarehouse(QWidget):
+    def __init__(self):
+        super(ProteoformWarehouse, self).__init__()
+        self.uisetup()
+        self.gui.filenamebox.setReadOnly(True)
+        self.h = 1
+
+        self.gui.inputFileButton.clicked.connect(self.on_inputfile_clicked)
+        self.gui.addAccession.clicked.connect(self.on_addAccession_clicked)
+        self.gui.deleteAccession.clicked.connect(self.on_deleteAccession_clicked)
+        self.gui.allAccessionGenerate.clicked.connect(self.runscript)
+        self.gui.selectAccessionGenerate.clicked.connect(self.runmodifiedscript)
+        self.gui.combinedxmlButton.clicked.connect(self.combinedxml)
+
+
+
+    def uisetup(self):
+        loader = QUiLoader()
+        path = os.path.join(os.path.dirname(__file__), "form.ui")
+        ui_file = QFile(path)
+        ui_file.reset()
+        ui_file.open(QFile.ReadOnly)
+        self.gui = loader.load(ui_file, self)
+        ui_file.close()
+
+
+    def on_inputfile_clicked(self):
+
+        filename, _ = QFileDialog.getOpenFileName(self, "Select Input File",
+                                                          os.getenv('HOME'), "*csv")
+
+        self.gui.filenamebox.setReadOnly(False)
+        self.gui.filenamebox.setText(str(filename))
+        self.gui.filenamebox.setReadOnly(True)
+
+    def on_addAccession_clicked(self):
+        g = self.gui.listWidget.count()
+        self.gui.listWidget.insertItem(g,"<Enter Accession Number Here>")
+        entry = self.gui.listWidget.item(g)
+        entry.setFlags(entry.flags() | QtCore.Qt.ItemIsEditable)
+
+    def on_deleteAccession_clicked(self):
+        if (self.gui.listWidget.count() > 1):
+            self.gui.listWidget.takeItem(self.gui.listWidget.count()-1)
+
+
+    def runscript(self):
+        os.system('python3 pro.py')
+
+    def runmodifiedscript(self):
+        command = 'python3 pro.py '
+        numfiles = self.gui.listWidget.count()
+        for x in range(1,numfiles):
+            placeholder = self.gui.listWidget.item(x)
+            command = command + str(placeholder.text()) + ' '
+        
+        os.system(command)
+
+    def combinedxml(self):
+        goal = 'python3 pro.py '
+        os.system(goal)
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = ProteoformWarehouse()
+    widget.show()
+    sys.exit(app.exec_())
---
+++ ProteoformWarehouse/pro.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import csv
+import pprint
+import requests
+import pandas as pd
+
+###########################
+#This drops all of the rows that have an NA in the modifications thus no mods to XML
+datafile2 = pd.read_csv('AllProteinGroups.csv')
+datafile3 = datafile2.loc[datafile2['Modification Info List'].dropna().index]
+ 
+#This function will be applied row by row and clean the modification column    
+def process_modifications(dataframe_accession, dataframe_column_split):
+    df = pd.DataFrame(columns = ['accession', 'aa#', 'modification', 'aa'])
+    for item in dataframe_column_split:
+        if 'DECOY' in dataframe_accession:
+            continue
+        else:
+            modification_info = []
+            modification_info.append(dataframe_accession)
+        if 'Fe' in item:
+            data = item.split(',info')[0]
+            aa = data.split('[')[0]
+            modification = data.split('[',1)[1].split(' on ')[0]
+            site = data.split('[',1)[1].split(' on ')[1]
+            modification_info.append(aa)
+            modification_info.append(modification)
+            modification_info.append(site)
+            df_length = len(df)
+            df.loc[df_length] = modification_info
+        else:
+            data = item.split(',info')[0]
+            aa = data.split('[')[0]
+            mod = data.split('[')[1]
+            modification = mod.split(' on ')[0]
+            site = mod.split(' on ')[1]
+            aa = aa.strip('#aa')
+            modification_info.append(aa)
+            modification_info.append(modification)
+            modification_info.append(site)
+            df_length = len(df)
+            df.loc[df_length] = modification_info
+    return df
+   
+   
+#Applying the function to the dataframe
+final_df = pd.concat(datafile3.apply(lambda x: process_modifications(x['Protein Accession'], x['Modification Info List'].split(';')),axis=1).values.tolist())
+
+
+#Dictionary of AA and their full name
+residue_dictionary = {'A':'Alanine','R':'Arginine','N':'Asparagine',
+                      'D':'Aspartic acid','C':'Cysteine','E':'Glutamic Acid',
+                      'Q':'Glutamine','G': 'Glycine','H':'Histidine','I': 'Isoleucine',
+                      'L':'Leucine','K':'Lysine','M':'Methionine','F':'Phenylalanine',
+                      'P':'Proline','S':'Serine','T':'Threonine','W':'Tryptophan',
+                      'Y':'Tyrosine','V': 'Valine'}
+
+#Add full aa name to dataframe
+final_df['aa_name'] = final_df.apply(lambda x: residue_dictionary[x['aa']],axis=1)
+
+def aa_sub_split(modification_entry):
+    if '->' in modification_entry:
+        l = modification_entry.split('->')[1]
+        return l
+    else:
+        return modification_entry
+
+final_df['modification'] = final_df.apply(lambda x: aa_sub_split(x['modification']),axis=1)
+
+#Request from uniprot and write the XML files. Need to edit this because it does pull down 0 xmls
+#Can also add the xml edits from below in the function
+def uniprot_request(accession_list):
+    for accession in accession_list:
+        if 'DECOY' in accession:
+            continue
+        else:
+            xml_file = accession + '.xml'
+            f = open(xml_file, 'w+')
+            url = 'https://www.uniprot.org/uniprot/%s.xml'  % accession
+            result = requests.get(url)
+            f.write(result.text)
+            f.close()
+   
+
+
+
+if os.path.isdir("appended_xml_files") == False:
+    os.mkdir("appended_xml_files")
+    os.chdir("appended_xml_files")
+else:
+    os.chdir("appended_xml_files")
+
+unique_accessions = final_df.accession.unique()
+
+uniprot_request(unique_accessions)
+
+for value in unique_accessions:
+    df = final_df[final_df['accession'] == value]
+    f = open((value + '.xml'), 'r')
+    first_line = f.readline()
+    if first_line == '':
+        f.close()
+        continue
+    else:
+        f.close()
+
+
+
+
+###########################
